2a63f996d5b7c295360ca8b23a0a9249f5b9fb65
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2021-03-18 16:35:52 -0300

    add Database and conncetion

--- backend/ProEventos/API/Controllers/EventoController.cs
+++ backend/ProEventos/API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
-﻿using API.Models;
+﻿using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -8,48 +9,26 @@ using System.Threading.Tasks;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class EventoController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class EventoController : ControllerBase
+  {
+    private readonly DataContext _context;
+    public EventoController(DataContext context)
     {
-
-        public IEnumerable<Evento> _evento = new Evento[]
-        {
-            new Evento()
-            {
-                EventoId = 1,
-                Tema ="Naruto",
-                Local = "Salto",
-                Lote = "1 lote",
-                QtdPessoas = 100,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemUrl = "https://static.wikia.nocookie.net/naruto-pedia/images/e/ea/Naruto.png/revision/latest?cb=20120407114822&path-prefix=pt-br"
-            },
-            new Evento()
-            {
-                EventoId = 2,
-                Tema ="DBZ",
-                Local = "Itu",
-                Lote = "2 lote",
-                QtdPessoas = 200,
-                DataEvento = DateTime.Now.AddDays(9).ToString("dd/MM/yyyy"),
-                ImagemUrl = "https://wpobservatoriodeseries.elav.tmp.br/wp-content/uploads/2020/05/Novo-Projeto-65.jpg"
-            },
-        };
-        public EventoController()
-        {
-        }
-
-        [HttpGet]
-        public IEnumerable<Evento> Get()
-        {
-            return _evento;
-        }
-
-        [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById(int id)
-        {
-            return _evento.Where(evento => evento.EventoId == id);
-        }
+      _context = context;
     }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+      return _context.Eventos;
+    }
+
+    [HttpGet("{id}")]
+    public Evento GetById(int id)
+    {
+      return _context.Eventos.FirstOrDefault(evento => evento.EventoId == id);
+    }
+  }
 }
