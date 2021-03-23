df970136ca30dc3a17ab104c906caecf88c873a9
Vinicius Fernandes <viniciusclementefernandes@gmail.com>
2021-03-23 16:14:24 -0300

    update Frontend - Eventos Component

--- frontend/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -9,6 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
+  larguraImagem: number = 100;
+  margemImagem: number = 2;
+  exibirImagem: boolean = true;
+  private _filtroLista: string = '';
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  public get filtroLista(): string {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  mostrarImagem(): void {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): any {
     this.http.get('http://localhost:5000/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response,
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     )
   }
